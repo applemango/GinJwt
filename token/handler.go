1ff5dbc55c3af56682ed391eb4d3a37bacf2de85
applemango <apple.mangoooo97@gmail.com>
2022-11-11 15:09:29 +0900

    change

--- token/handler.go
+++ token/handler.go
@@ -11,7 +11,6 @@ import (
 
 func Login(username string, password string) (map[string]string, error) {
 	user, err := models.GetUserFromUsername(username)
-	//user, err := models.GetUserFromId(1)
 	if err != nil {
 		return nil, errors.New("user not found or invalid password")
 	}
@@ -29,26 +28,15 @@ func Login(username string, password string) (map[string]string, error) {
 }
 
 func RefreshToken(refresh_token string) (string, error) {
-
-	// Parse takes the token string and a function for looking up the key.
-	// The latter is especially useful if you use multiple keys for your application.
-	// The standard is to use 'kid' in the head of the token to identify
-	// which key to use, but the parsed token (head and claims) is provided
-	// to the callback, providing flexibility.
 	token, err := jwt.Parse(refresh_token, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
-
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
 		return []byte("secret"), nil
 	})
 
 	fmt.Println(refresh_token, token, err)
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		// Get the user record from database or
-		// run through your business logic to verify if the user can log in
 
 		user, error := models.GetUserFromId(int(claims["sub"].(float64)))
 		if error != nil {
--- token/token.go
+++ token/token.go
@@ -7,44 +7,30 @@ import (
 )
 
 func GenerateTokenAccess(sub int, name string) (string, error) {
-	// Create token
 	token := jwt.New(jwt.SigningMethodHS256)
 
-	// Set claims
-	// This is the information which frontend can use
-	// The backend can also decode the token and get admin etc.
 	claims := token.Claims.(jwt.MapClaims)
 	claims["sub"] = sub
 	claims["name"] = name
 	claims["refresh"] = false
 	claims["exp"] = time.Now().Add(time.Minute * 15).Unix()
 
-	// Generate encoded token and send it as response.
-	// The signing string should be secret (a generated UUID works too)
 	t, err := token.SignedString([]byte("secret"))
 	if err != nil {
-		// return nil?
 		return "", err
 	}
-	//fmt.Println("secret: ", t)
 	return t, nil
 }
 
 func GenerateTokenPair(sub int, name string) (map[string]string, error) {
-	// Create token
 	token := jwt.New(jwt.SigningMethodHS256)
 
-	// Set claims
-	// This is the information which frontend can use
-	// The backend can also decode the token and get admin etc.
 	claims := token.Claims.(jwt.MapClaims)
 	claims["sub"] = sub
 	claims["name"] = name
 	claims["refresh"] = false
 	claims["exp"] = time.Now().Add(time.Minute * 15).Unix()
 
-	// Generate encoded token and send it as response.
-	// The signing string should be secret (a generated UUID works too)
 	t, err := token.SignedString([]byte("secret"))
 	if err != nil {
 		return nil, err
